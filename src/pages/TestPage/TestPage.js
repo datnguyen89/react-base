f54752115ecd9e671e8232df0207faad37cc8569
datnt94 <datnt94@vpbank.com.vn>
2023-04-25 14:32:47 +0700

    .

--- src/pages/TestPage/TestPage.js
+++ src/pages/TestPage/TestPage.js
@@ -2,8 +2,9 @@ import React, { useContext, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { FormItemWrapper, TestPageWrapper } from './TestPageStyled'
 import { LoadingContext } from 'react-router-loading'
-import * as Yup from 'yup';
+import * as Yup from 'yup'
 import { Button, Col, Form, Input, Row } from 'antd'
+import { ErrorMessage, Field, Formik } from 'formik'
 
 
 const TestPage = props => {
@@ -25,7 +26,7 @@ const TestPage = props => {
   return (
     <TestPageWrapper>
       <Form onFinish={handleFinish}>
-        <Row gutter={[8,8]}>
+        <Row gutter={[8, 8]}>
           <Col span={10}>
             <FormItemWrapper>
               <label htmlFor={'Username'}>Tên đăng nhập</label>
@@ -46,15 +47,46 @@ const TestPage = props => {
             <Button type={'primary'} htmlType={'submit'}>Submit</Button>
           </Col>
         </Row>
-
-
       </Form>
+      <br />
+      <Formik
+        initialValues={{ firstName: '', lastName: '', email: '' }}
+        validationSchema={Yup.object({
+          firstName: Yup.string()
+            .max(15, 'Must be 15 characters or less')
+            .required('Required'),
+          lastName: Yup.string()
+            .max(20, 'Must be 20 characters or less')
+            .required('Required'),
+          email: Yup.string().email('Invalid email address').required('Required'),
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        <Form>
+          <label htmlFor="firstName">First Name</label>
+          <Field name="firstName" type="text" />
+          <ErrorMessage name="firstName" />
+
+          <label htmlFor="lastName">Last Name</label>
+          <Field name="lastName" type="text" />
+          <ErrorMessage name="lastName" />
+
+          <label htmlFor="email">Email Address</label>
+          <Field name="email" type="email" />
+          <ErrorMessage name="email" />
+
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
     </TestPageWrapper>
   )
 }
 
-TestPage.propTypes = {
-
-}
+TestPage.propTypes = {}
 
 export default TestPage
